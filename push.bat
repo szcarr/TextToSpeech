188fb644a1005df5c4666a57373db047b6e63844
szcarr <itwangzy@gmail.com>
2022-02-09 17:47:56 +0100

    New version

--- push.bat
+++ push.bat
@@ -5,6 +5,6 @@ git add .
 git commit -m "New version"
 git remote rm origin
 git remote add origin https://github.com/szcarr/TextToSpeech.git
-git pull --rebase origin main
+git pull origin main
 git push -f origin main
 pause
